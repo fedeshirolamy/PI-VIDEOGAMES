07741f776a13ff9794ff881c57c68fcb6d7f1c93
Your Name <you@example.com>
2022-07-16 13:36:14 -0300

    finish

--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -22,11 +22,11 @@ export default function Detail(props) {
                     <div>
                         <h3>{myVideogame.name}</h3>
                         <img src={myVideogame.background_image} alt="" width='300px' height='300px' />
-                        <h5>Genres:</h5>{myVideogame.genres}
+                        <h5>Genres:</h5>{myVideogame.genres + ' '}
                         <h5>Platforms:</h5>
-                        <ul>{myVideogame.platforms}</ul>
+                        <ul>{myVideogame.platforms + ', '}</ul>
                         <h4>Fecha de lanzamiento: {myVideogame.releaseDate}</h4> 
-                        <h4>Rating: <h5>{myVideogame.rating}</h5></h4>
+                        <h4>Rating: {myVideogame.rating}</h4>
                         <h5>Description:</h5>
                         <p>{myVideogame.description}</p>
                     </div> :
